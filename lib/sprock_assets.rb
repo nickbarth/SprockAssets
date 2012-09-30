7fc76a45c39e683070975300bb916d94a4560b63
Nick Barth <nickbarth@live.com>
2012-09-30 04:14:59 +0000

    updated function documentation

--- lib/sprock_assets.rb
+++ lib/sprock_assets.rb
@@ -2,8 +2,17 @@ require 'sprockets'
 require 'sprock_assets/version'
 
 class SprockAssets
-  attr_accessor :app, :settings, :assets
+  # Public: Gets/Sets the Rack application instance.
+  attr_accessor :app
+  # Public: Gets/Sets the middleware settings.
+  attr_accessor :settings
+  # Public: Gets/Sets the Sprockets Enviroment.
+  attr_accessor :assets
 
+  # Public: Initialize SprockAssets a middleware.
+  #
+  # app - The current Rack app.
+  # settings - A hash of the asset path settings.
   def initialize(app, settings={})
     @app = app
     @settings = {
@@ -20,6 +29,7 @@ class SprockAssets
     end
   end
 
+  # Public: A rack middleware call method routing URLs under /assets/ to the SprockAssets middleware.
   def call(env)
     if env['PATH_INFO'][/^\/assets/].nil?
       @app.call env
--- lib/sprock_assets/version.rb
+++ lib/sprock_assets/version.rb
@@ -1,3 +1,4 @@
 class SprockAssets
+  # Public: String version number of SprockAsset gem.
   VERSION = '0.0.1'
 end
